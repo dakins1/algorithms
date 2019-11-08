2e7910814b19f10d676d384e19f248cb8d4a854c
Dillon Akins <dakins1@trinity.edu>
2019-11-08 14:42:05 -0600

    algies

---
+++ maxFlow/maxFlow.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+vector<tuple<int,int,int>> courierLoads(
+	const vector<int> &orders, 
+	const vector<tuple<int,int,int>> &capacities)
+{
+	cout << orders << endl;	
+	cout << capacities << endl;
+	
+}
+
+
+//void main() {
+    
+//}
